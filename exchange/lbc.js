13ec37b8da79da30972fa64b03d60013d32e10f2
Muhammad Wasim <muhammad.wasim@zigron.com>
2017-11-13 11:59:33 +0500

    testing

--- exchange/lbc.js
+++ exchange/lbc.js
@@ -7,6 +7,7 @@ const config = require('config');
 const url = "https://localbitcoins.com/";
 const requestDefaultOptions = {json:true}
 const Q = require('q');
+var a="test";
 
 
 var resolve = function(q,options) {
