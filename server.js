e7ea312e2e012cada4e8f668dfaf120395388d57
Muhammad Wasim <muhammad.wasim@zigron.com>
2017-11-13 11:23:50 +0500

    changing port

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use("/api",apis);
 
 // Listen to this Port
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(3005,function(){
+  console.log("Live at Port 3005");
 });
